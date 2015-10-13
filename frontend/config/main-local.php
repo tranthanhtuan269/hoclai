fcb0d3c1105fbacd4e02eb2f809669739ad08c16
tran.thanh.tuan269 <tran.thanh.tuan269@gmail.com>
2015-10-13 14:28:57 +0700

    add ignore file

---
+++ frontend/config/main-local.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 'ynoe1iysrvAtl9NTKdKRMQq1NEt7HnAn',
+        ],
+    ],
+];
